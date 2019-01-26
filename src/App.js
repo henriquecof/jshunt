eb7eb3d7025181040fbb8265acfbcffd6b109b04
Henrique Almeida <almeida.cof@gmail.com>
2019-01-26 11:16:30 -0300

    final curso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
+import Routes from './routes';
 import "./styles.css";
 import Header from "./components/Header/index";
 import Main from './pages/main';
+import Route from "react-router-dom/Route";
 
 const App = () =>(
   <div className="App">
     <Header />
-    <Main /> 
+    <Routes /> 
   </div>
 );
 
